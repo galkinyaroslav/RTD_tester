3e930d376ab32ffb61cfb79ea5e6cc328d8a54a5
Yaroslav Galkin <galkinyaroslav@gmail.com>
2025-10-09 11:36:01 +0300

    add device controller class

--- src/meas_control.py
+++ src/meas_control.py
@@ -1,28 +1,121 @@
+import logging
 import time
 
 import pyvisa
 
-CHANNELS = '(@205:210)'
-DEVISE_NAME = '34970A'
 
-class PT100Controller:
-    def __init__(self):
+logger = logging.getLogger("visa_client")
+
+# class PT100Controller:
+#     def __init__(self):
+#         self.rm = None
+#         self.instrument = None
+#         self.connected = False
+#
+#     def connect(self, visa_address="GPIB0::22::INSTR"):
+#         """Подключение к Agilent 34970"""
+#         try:
+#             self.rm = pyvisa.ResourceManager()
+#             self.instrument = self.rm.open_resource(visa_address)
+#             self.instrument.timeout = 10000
+#             self.connected = True
+#             logger.info(f"Успешно подключено к {visa_address}")
+#             return True
+#         except Exception as e:
+#             logger.error(f"Ошибка подключения: {e}")
+#             return False
+#
+#     def disconnect(self):
+#         """Отключение от прибора"""
+#         if self.instrument:
+#             self.instrument.close()
+#         if self.rm:
+#             self.rm.close()
+#         self.connected = False
+#         logger.info("Отключено от прибора")
+#
+#     def configure_measurement(self, channels=["101", "102", "103"]):
+#         """Настройка измерения температуры PT100"""
+#         try:
+#             # Сброс прибора
+#             self.instrument.write("*RST")
+#             time.sleep(1)
+#
+#             # Настройка для PT100 (4-проводное подключение)
+#             for channel in channels:
+#                 # FRES - 4-проводное измерение сопротивления
+#                 self.instrument.write(f"CONF:FRES {channel}")
+#                 # Диапазон 100 Ом для PT100
+#                 self.instrument.write(f"FRES:RANGE 100, {channel}")
+#                 # Разрешение 0.001 Ом
+#                 self.instrument.write(f"FRES:RES 0.001, {channel}")
+#                 # NPLC = 1
+#                 self.instrument.write(f"FRES:NPLC 1, {channel}")
+#
+#             logger.info(f"Настроены каналы: {channels}")
+#             return True
+#         except Exception as e:
+#             logger.error(f"Ошибка настройки: {e}")
+#             return False
+#
+#     def read_temperature(self, channels=["101", "102", "103"]):
+#         """Чтение температуры с каналов"""
+#         try:
+#             measurements = {}
+#             for channel in channels:
+#                 # Чтение сопротивления
+#                 resistance = float(self.instrument.query(f"READ? {channel}"))
+#                 # Конвертация сопротивления в температуру
+#                 temperature = self.resistance_to_temperature(resistance)
+#                 measurements[channel] = {
+#                     'resistance': round(resistance, 4),
+#                     'temperature': temperature,
+#                     'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#                 }
+#             return measurements
+#         except Exception as e:
+#             logger.error(f"Ошибка чтения: {e}")
+#             return None
+
+
+class DAQ_34970A():
+    def __init__(self, channels: list = None):
+        self.channels = ['205','206','207','208','209','210',] if not channels else self.make_channels_str(channels) # need to change
+        self.__device_name = '34970A'
         self.rm = None
         self.instrument = None
         self.connected = False
+        self.visa_address: str = ''
 
-    def connect(self, visa_address="GPIB0::22::INSTR"):
-        """Подключение к Agilent 34970"""
-        try:
-            self.rm = pyvisa.ResourceManager()
-            self.instrument = self.rm.open_resource(visa_address)
-            self.instrument.timeout = 10000
-            self.connected = True
-            logger.info(f"Успешно подключено к {visa_address}")
-            return True
-        except Exception as e:
-            logger.error(f"Ошибка подключения: {e}")
-            return False
+    def make_channels_str(self, channels: list)-> str:
+        return '(@'+','.join(channels)+')'
+
+    def connect(self) -> bool | None:
+        self.rm = pyvisa.ResourceManager()  # '/usr/lib/x86_64-linux-gnu/libiovisa.so'
+        list_devices = self.rm.list_resources()
+        # print(list_devices)
+        for visa_address in list_devices:
+            try:
+                temp_device = self.rm.open_resource(visa_address)
+                temp_name = temp_device.query('*IDN?')
+                if self.__device_name in temp_name:
+                    # print(temp_name.strip(), '--->>>', visa_address)
+                    # temp_device.close()
+                    logger.info(f"Successfully connected to {visa_address}")
+                    self.instrument = temp_device
+                    self.connected = True
+                    self.visa_address = visa_address
+                    return True
+                else:
+                    temp_device.close()
+                    logger.info(f"Not appropriate device {visa_address}")
+
+
+            except pyvisa.errors.VisaIOError as e:
+                # print('There is not appropriate device or device is not found')
+                logger.error(f"This is not appropriate device or device is not found: {e}")
+                return False
+        return None
 
     def disconnect(self):
         """Отключение от прибора"""
@@ -31,121 +124,39 @@ class PT100Controller:
         if self.rm:
             self.rm.close()
         self.connected = False
-        logger.info("Отключено от прибора")
+        logger.info(f"Disconnected from {self.visa_address} ")
 
-    def configure_measurement(self, channels=["101", "102", "103"]):
-        """Настройка измерения температуры PT100"""
+    def configure(self):
         try:
-            # Сброс прибора
-            self.instrument.write("*RST")
+            # Reset instrument with delay!!!
+            self.instrument.write('*RST')
             time.sleep(1)
 
-            # Настройка для PT100 (4-проводное подключение)
-            for channel in channels:
-                # FRES - 4-проводное измерение сопротивления
-                self.instrument.write(f"CONF:FRES {channel}")
-                # Диапазон 100 Ом для PT100
-                self.instrument.write(f"FRES:RANGE 100, {channel}")
-                # Разрешение 0.001 Ом
-                self.instrument.write(f"FRES:RES 0.001, {channel}")
-                # NPLC = 1
-                self.instrument.write(f"FRES:NPLC 1, {channel}")
+            self.instrument.write('*CLS')
+            self.instrument.write_termination = '\r\n'
+            self.instrument.read_termination = '\r\n'
 
-            logger.info(f"Настроены каналы: {channels}")
+            self.instrument.write(f'CONF:TEMP FRTD, 85, (@{','.join(self.channels)})')
+            self.instrument.write(f'TEMP:TRAN:FRTD:RES:REF 100, (@{','.join(self.channels)})')
+            self.instrument.write(f'TEMP:TRAN:FRTD:TYPE 85, (@{','.join(self.channels)})')
+
+            self.instrument.write(f'ROUT:SCAN (@{','.join(self.channels)})')
+
+            logger.info(f"Channels: {self.channels} are configured")
             return True
         except Exception as e:
-            logger.error(f"Ошибка настройки: {e}")
+            logger.error(f"Configuration error : {e}")
             return False
 
-    def read_temperature(self, channels=["101", "102", "103"]):
-        """Чтение температуры с каналов"""
-        try:
-            measurements = {}
-            for channel in channels:
-                # Чтение сопротивления
-                resistance = float(self.instrument.query(f"READ? {channel}"))
-                # Конвертация сопротивления в температуру
-                temperature = self.resistance_to_temperature(resistance)
-                measurements[channel] = {
-                    'resistance': round(resistance, 4),
-                    'temperature': temperature,
-                    'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                }
-            return measurements
-        except Exception as e:
-            logger.error(f"Ошибка чтения: {e}")
-            return None
-
-
-def find_device() -> pyvisa.resources.Resource | None:
-    rmag = pyvisa.ResourceManager()  # '/usr/lib/x86_64-linux-gnu/libiovisa.so'
-    list_devices = rmag.list_resources()
-    print(list_devices)
-    for device in list_devices:
-        try:
-            temp_device = rmag.open_resource(device)
-            temp_name = temp_device.query('*IDN?')
-            if DEVISE_NAME in temp_name:
-                print(temp_name.strip(), '--->>>', device)
-                # temp_device.close()
-                return temp_device
-
-        except pyvisa.errors.VisaIOError:
-            print('This is not appropriate device or device is not found')
-
-
-def configure(device: pyvisa.resources.Resource) -> dict:
-
-    device.write('*RST')
-    time.sleep(1)
-    device.write('*CLS')
-    device.write_termination = '\r\n'
-    device.read_termination = '\r\n'
-    device.write(f'CONF:TEMP FRTD, 85, {CHANNELS}')
-    device.write(f'TEMP:TRAN:FRTD:RES:REF 100, {CHANNELS}')
-
-    # print(device.query(f'TEMP:TRAN:FRTD:RES:REF? {CHANNELS}'))
-
-    device.write(f'TEMP:TRAN:FRTD:TYPE 85, {CHANNELS}')
-    # print(device.query(f'TEMP:TRAN:FRTD:TYPE? {CHANNELS}'))
-
-    # print(device.query(f'CONF? {CHANNELS}'))
-    device.write(f'ROUT:SCAN {CHANNELS}')
-    message = f"{device.query('*IDN?')} --->>> CONFIGURED"
-    print(message)
-    return {'message': message}
-
-
-def read_data(device:  pyvisa.resources.Resource) -> dict:
-    device.write(f'INIT')
-    row_data = device.query(f'FETC?')
-    # data = [float(value) for value in row_data.strip().split(',')]
-    # print(data)
-    return {'data': [float(i) for i in row_data.split(',')]}
+    def read_data(self) -> dict:
+        self.instrument.write(f'INIT')
+        raw_data = self.instrument.query(f'FETC?')
+        return dict(zip(self.channels,[float(i) for i in raw_data.split(',')]))
 
 if __name__ == '__main__':
-    from datetime import datetime
-    import os
-    # def save_data(d: dict) -> None:
-    #     path = './saved_arkolab'
-    #     if not os.path.exists(path):
-    #         os.makedirs(path)
-    #     filename = f'{path}/RT{str(datetime.now())}.csv'
-    #     # wb.save(filename)
-    #     # wb.close()
-    #     with open(filename, 'w') as f:
-    #         for key, value in d.items():
-    #             f.write(str(key) + ',' + ','.join(str(a) for a in value) + '\n')
 
-
-    a34970 = find_device()
-    # configure(a34970)
-    data_dict = {}
-    # time_tick = 3
-    # for i in range(31):
-    #     data_dict[i*time_tick] = read_data(a34970)['data']
-    #     print(data_dict[i*time_tick])
-    #     time.sleep(time_tick)
-    # save_data(data_dict)
-    print(read_data(a34970))
-    a34970.close()
+    device = DAQ_34970A()
+    device.connect()
+    device.configure()
+    print(device.read_data())
+    device.disconnect()

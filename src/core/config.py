4216dc9d22143678e5e27f45338ccac4e5ee3c80
galkinyaroslav <galkinyaroslav@gmail.com>
2025-10-10 23:38:12 +0300

    add pydantic settings for .env

--- src/core/config.py
+++ src/core/config.py
@@ -1,32 +1,36 @@
 from pathlib import Path
 
-from dotenv import load_dotenv
-import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
 
-load_dotenv()
-#
-DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
-DB_NAME = os.environ.get('DB_NAME')
-DB_USER = os.environ.get('DB_USER')
-DB_PASS = os.environ.get('DB_PASS')
-DB_URL = os.environ.get('DB_URL')
-# SECRET_KEY_PUBLIC = os.environ.get('SECRET_KEY_PUBLIC')
-# SECRET_KEY_PRIVATE = os.environ.get('SECRET_KEY_PRIVATE')
-# SECRET_KEY_HS256 = os.environ.get('SECRET_KEY_HS256')
-# SECRET_KEY_RESET = os.environ.get('SECRET_KEY_RESET')
-# SECRET_KEY_VERIFICATION = os.environ.get('SECRET_KEY_VERIFICATION')
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+    DB_PGA_EMAIL: str
+    DB_PGA_PASSWORD: str
+    DB_PGA_PORT: int
+    DB_URL: str
+    DEBUG: bool
+
+    model_config = SettingsConfigDict(env_file=Path(BASE_DIR,'.env'), env_file_encoding='utf-8')
+
+
+# .env is runed once and cashed
+@lru_cache()
+def get_settings() -> Settings:
+    return Settings()
+
+settings = get_settings()
+
 
 if __name__ == '__main__':
-    print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     print(f'{BASE_DIR=}')
 
-    print(f'DB_HOST: {DB_HOST}\n',
-          f'DB_PORT: {DB_PORT}\n',
-          f'DB_NAME: {DB_NAME}\n',
-          f'DB_USER: {DB_USER}\n',
-          f'DB_PASS: {DB_PASS}\n',
-          f'DB_URL: {DB_URL}\n')
+    # settings = get_settings()
+    print(settings)

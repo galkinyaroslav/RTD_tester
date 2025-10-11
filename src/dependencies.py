471f48584757d0b61228d35a99544dcc2307320f
galkinyaroslav <galkinyaroslav@gmail.com>
2025-10-12 00:36:09 +0300

    dependencies add templates

--- src/dependencies.py
+++ src/dependencies.py
@@ -3,27 +3,33 @@ from fastapi import Request
 
 def get_measurement_state(request: Request):
     """
-    Dependency, возвращающая MeasurementState из app.state
+    Dependency, return MeasurementState from app.state
     """
     return request.app.state.measurement
 
 def get_ws_connection_manager_state(request: Request):
     """
-    Dependency, возвращающая ConnectionManager из app.state
+    Dependency, return ConnectionManager from app.state
     """
     return request.app.state.ws_connection_manager
 
 def get_instrument_state(request: Request):
     """
-    Dependency, возвращающая DAQ_34970A из app.state
+    Dependency, return DAQ_34970A from app.state
     """
     return request.app.state.instrument
 
 def get_dot_env_state(request: Request):
     """
-    Dependency, возвращающая данные .env из app.state
+    Dependency, return .env data from app.state
     """
     return request.app.state.dot_env
+
+def get_templates_state(request: Request):
+    """
+    Dependency, return templates from app.state
+    """
+    return request.app.state.templates
 
 
 

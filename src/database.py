5e9f40a1c2bc35c88363b9989f18ee055dd3c7cb
galkinyaroslav <galkinyaroslav@gmail.com>
2025-10-12 00:39:08 +0300

    add DB to app.state

--- src/database.py
+++ src/database.py
@@ -1,21 +1,21 @@
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
+from fastapi import Request
 
-from src.core.config import DB_NAME, DB_PORT, DB_HOST, DB_PASS, DB_USER
-
-DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+# DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 
 class Base(DeclarativeBase):
     __table_args__ = {'extend_existing': True}
 
 
-engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+# engine = create_async_engine(DATABASE_URL)
+# db_sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
 
 
-async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+async def get_async_session(request: Request) -> AsyncGenerator[AsyncSession, None]:
+    async_session_maker = request.app.state.db_sessionmaker
     async with async_session_maker() as session:
         yield session
 
